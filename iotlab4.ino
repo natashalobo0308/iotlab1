58167446ae07dbba8a5e00b81f61f6784cfe768f
natashalobo0308 <143173229+natashalobo0308@users.noreply.github.com>
2025-09-08 15:29:30 +0530

    Add files via upload

---
+++ iotlab4.ino
@@ -0,0 +1,26 @@
+int value=0;
+void setup()
+{
+  
+  pinMode(A1,INPUT);
+  pinMode(5,OUTPUT);
+  pinMode(7,OUTPUT);
+  Serial.begin(9600);
+}
+void loop()
+{
+  value=analogRead(A1);
+  Serial.println(value);
+  if(value<200){
+    digitalWrite(5,LOW);
+    digitalWrite(7,LOW);
+  }
+  else if(value>200 && value<400){
+    digitalWrite(5,HIGH);
+    digitalWrite(7,LOW);
+  }
+  else if(value>400){
+    digitalWrite(5,HIGH);
+    digitalWrite(7,HIGH);
+  }
+}
---
+++ iotlab5.ino
@@ -0,0 +1,27 @@
+// C++ code
+//
+int value=0;
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(13, OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  value=analogRead(A0);
+  Serial.print(value);
+  if(value<=600){
+    digitalWrite(13, LOW);
+    Serial.println(":Very wet");
+    
+  }else if(value>600 && value<800){
+    Serial.println(":Medium");
+  }
+  else if(value>=800){
+    digitalWrite(13, HIGH);
+    Serial.println(":Very dry");
+  }
+}
